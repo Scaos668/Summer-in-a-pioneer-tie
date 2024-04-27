faa7e922369b47fc05ec7fd84d818f91dddfc8f2
Scaos668 <dobronravov.denison@gmail.com>
2024-04-27 21:56:49 +0300

    part-working_scene_change

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from scripts import scenes, settings, music
+import string
+
+from scripts import scenes, settings, music, texts
 import pygame
 
 working = True
@@ -8,22 +10,64 @@ lastTiming = -100
 
 isMenu = True
 
+scriptLineScene = 0
+currSceneText = 0
+
+textBack = pygame.image.load('textures/TextBack.png')
+
+lastLetter = clock.get_time()
+
+done = False
+currIndex = 0
+currText = ''
+
+allText = ''
 
 while working:
-    if isMenu:
-        scenes.loadScene("scenes/menu/back.png")
+    if clock.get_time()-lastTiming > 10:
+        if isMenu:
+            music.play_ambient_song()
+            lastTiming = clock.get_time()
+        else:
+            pygame.mixer.music.stop()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             working = False
 
-        # if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-        #     scenes.loadScene('scenes/mainScenes/bus.jpg')
-        #     isMenu = False
+        if currIndex < settings.letterinterval * len(allText):
+            currIndex += 1
+        elif currIndex >= settings.letterinterval * len((allText)):
+            done = True
 
-    if clock.get_time()-lastTiming > 100:
-        music.play_ambient_song()
-        lastTiming = clock.get_time()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            isMenu = False
+
+            data = scenes.nextText(currSceneText, scriptLineScene)
+
+            currSceneText = data[0]
+            scriptLineScene = data[1]
+
+            scenes.loadScene(scriptLineScene)
+
+            scenes.screen.blit(textBack, (settings.VW / 2 - textBack.get_width() / 2, settings.VH / 2 + textBack.get_height() / 3))
+
+            allText = texts.textToShow[scriptLineScene][currSceneText]
+
+            currIndex = 0
+
+            done = False
+
+    if isMenu:
+        scenes.loadScene(0)
+
+    currText = texts.font.render(allText[0:currIndex//settings.letterinterval], False, 'white')
+
+    textArea = scenes.screen.blit(currText, (180,500))
+
+    if done and not isMenu:
+        contText = texts.font.render('Продолжить', False, 'white')
+        scenes.screen.blit(contText, (710, 625))
 
     pygame.display.flip()
     clock.tick(settings.FPS)
--- scripts/scenes.py
+++ scripts/scenes.py
@@ -4,11 +4,57 @@ from scripts import settings
 pygame.init()
 screen = pygame.display.set_mode((settings.VW, settings.VH))
 
-def loadScene(path):
 
-    img = pygame.image.load(path)
+def loadScene(currScene):
+    if currScene == 0:
+        img = pygame.image.load('scenes/menu/back.png')
+        screen.blit(img, (0, 0))
+    if currScene == 1:
+        img = pygame.image.load('scenes/mainScenes/bus.jpg')
+        screen.blit(img, (0, 0))
+    if currScene == 2:
+        img = pygame.image.load('scenes/menu/back.png')
+        screen.blit(img, (0, 0))
 
-    screen.blit(img, (0, 0))
+
+def nextText(currTextNum, currSceneNum):
+    if currSceneNum == 0:
+        return [0, 1]
+    elif currSceneNum == 1:
+        if currTextNum == 7:
+            currSceneNum += 1
+            currTextNum = 0
+        else:
+            currTextNum += 1
+    elif currSceneNum == 2:
+        if currSceneNum == 15:
+            currSceneNum += 1
+            currTextNum = 0
+        else:
+            currTextNum += 1
+    elif currSceneNum == 3:
+        pass
+    elif currSceneNum == 4:
+        pass
+    elif currSceneNum == 5:
+        pass
+    elif currSceneNum == 6:
+        pass
+    elif currSceneNum == 7:
+        pass
+    elif currSceneNum == 8:
+        pass
+    elif currSceneNum == 9:
+        pass
+    elif currSceneNum == 10:
+        pass
+    elif currSceneNum == 11:
+        pass
+
+
+    return currTextNum, currSceneNum
+
+
 
 
 
